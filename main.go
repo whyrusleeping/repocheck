d8785b20350e8aa205f47c96602b74f75f9fabff
Jeromy <jeromyj@gmail.com>
2015-12-13 11:16:20 -0800

    prints

--- main.go
+++ main.go
@@ -28,21 +28,30 @@ func (dl DirList) Swap(i, j int)      { dl[i], dl[j] = dl[j], dl[i] }
 func (dl DirList) Less(i, j int) bool { return dl[i].Count > dl[j].Count }
 
 func main() {
+	fmt.Printf("opening repo at %s\n", os.Args[1])
 	r, err := fsrepo.Open(os.Args[1])
 	if err != nil {
 		panic(err)
 	}
 
+	fmt.Println("repo opened, starting key channel")
 	bs := blockstore.NewBlockstore(r.Datastore())
 	kc, err := bs.AllKeysChan(context.Background())
 	if err != nil {
 		panic(err)
 	}
 
+	fmt.Println("key channel started. beginning processing")
+
 	t := make(map[string]int)
 	errtypes := make(map[string]int)
 	dirents := make(map[string]*Dirent)
+	var count int
 	for k := range kc {
+		count++
+		if count%1000 == 0 {
+			fmt.Printf("processed: %d\n", count)
+		}
 		blk, err := bs.Get(k)
 		if err != nil {
 			panic(err)
