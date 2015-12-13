10cf258ad5db1a33acd20e2057c633e675e6ad4f
Jeromy <jeromyj@gmail.com>
2015-12-13 11:05:04 -0800

    first pass

---
+++ main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"sort"
+	"strings"
+
+	"github.com/ipfs/go-ipfs/blocks/blockstore"
+	"github.com/ipfs/go-ipfs/blocks/key"
+	"github.com/ipfs/go-ipfs/merkledag"
+	"github.com/ipfs/go-ipfs/repo/fsrepo"
+	"github.com/ipfs/go-ipfs/unixfs"
+
+	"golang.org/x/net/context"
+)
+
+type Dirent struct {
+	Name  string
+	Hash  key.Key
+	Count int
+}
+
+type DirList []Dirent
+
+func (dl DirList) Len() int           { return len(dl) }
+func (dl DirList) Swap(i, j int)      { dl[i], dl[j] = dl[j], dl[i] }
+func (dl DirList) Less(i, j int) bool { return dl[i].Count > dl[j].Count }
+
+func main() {
+	r, err := fsrepo.Open(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+
+	bs := blockstore.NewBlockstore(r.Datastore())
+	kc, err := bs.AllKeysChan(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
+	t := make(map[string]int)
+	errtypes := make(map[string]int)
+	dirents := make(map[string]*Dirent)
+	for k := range kc {
+		blk, err := bs.Get(k)
+		if err != nil {
+			panic(err)
+		}
+
+		nd, err := merkledag.Decoded(blk.Data)
+		if err != nil {
+			errtypes["notdag"]++
+			continue
+		}
+
+		upb, err := unixfs.FromBytes(nd.Data)
+		if err != nil {
+			switch {
+			case strings.Contains(err.Error(), "proto:"):
+				errtypes["not unixfs"]++
+			default:
+				errtypes[err.Error()]++
+			}
+			continue
+		}
+
+		t[upb.GetType().String()]++
+
+		if upb.GetType() == unixfs.TDirectory {
+			for _, l := range nd.Links {
+				tag := l.Name + string(l.Hash)
+				e, ok := dirents[tag]
+				if !ok {
+					e = &Dirent{
+						Name: l.Name,
+						Hash: key.Key(l.Hash),
+					}
+					dirents[tag] = e
+				}
+				e.Count++
+			}
+		}
+	}
+
+	var dl DirList
+	for _, d := range dirents {
+		dl = append(dl, *d)
+	}
+	sort.Sort(dl)
+
+	for i := 0; i < 10; i++ {
+		fmt.Printf("%s %s %d\n", dl[i].Name, dl[i].Hash.B58String(), dl[i].Count)
+	}
+	fmt.Println(t)
+	fmt.Println(errtypes)
+}
